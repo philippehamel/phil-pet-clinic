07178099e55a0d27ee9634db87d1edb5affdcfee
Philippe Hamel <philippehamel85@gmail.com>
2025-03-25 21:58:48 -0400

    implement interface

--- pet-clinic-data/src/main/java/phil/springframework/services/OwnerService.java
+++ pet-clinic-data/src/main/java/phil/springframework/services/OwnerService.java
@@ -1,4 +1,12 @@
 package phil.springframework.services;
 
+import phil.springframework.model.Owner;
+
+import java.util.Set;
+
 public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
 }
---
+++ pet-clinic-data/src/main/java/phil/springframework/services/VetService.java
@@ -0,0 +1,11 @@
+package phil.springframework.services;
+
+import phil.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
