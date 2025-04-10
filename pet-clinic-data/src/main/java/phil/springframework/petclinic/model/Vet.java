8fefeb575f5b3ff2b65bf7d8eebe24b3aa1278e6
Philippe Hamel <philippehamel85@gmail.com>
2025-04-10 16:54:42 -0400

    add jpa annotations to pet and vet entity

--- pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Vet.java
@@ -7,9 +7,24 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 @Setter
 @Getter
 @NoArgsConstructor
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
+
 }
--- pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Visit.java
@@ -6,13 +6,25 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Visit extends BaseEntity{
 
+    @ManyToOne
     private Pet pet;
+
+    @Column(name = "visit_date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
 
 }
