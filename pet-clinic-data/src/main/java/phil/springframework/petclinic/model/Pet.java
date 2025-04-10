f3be310543c9827ecbe165f8fbf03720bdebc593
Philippe Hamel <philippehamel85@gmail.com>
2025-04-10 16:38:19 -0400

    add jpa annotations for owner entity

--- pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/phil/springframework/petclinic/model/Pet.java
@@ -1,18 +1,43 @@
 package phil.springframework.petclinic.model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
+@Entity
+@Table(name = "pets")
 @Setter
 @Getter
 @NoArgsConstructor
 public class Pet extends BaseEntity{
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthday;
-    //private Set<Visit> visits = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
 }
